9c84c693c73c588f714a2df9c31fece64d73fb32
clevanilson <clevanilson.contato@gmail.com>
2024-03-24 16:23:36 -0300

    test(cpf): add tests for valid and invalid CPF

---
+++ test/validadeCpf.test.ts
@@ -0,0 +1,17 @@
+import { validateCpf } from "../src/validateCpf";
+
+test.each(["97456321558", "71428793860", "87748248800"])(
+  "Shoud test a valid CPF: %s",
+  (cpf) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toEqual(true);
+  }
+);
+
+test.each(["11111111111", "7142879386", "87748248801", ""])(
+  "Should test an invalid CPF: %s",
+  (cpf) => {
+    const isValid = validateCpf(cpf);
+    expect(isValid).toEqual(false);
+  }
+);
