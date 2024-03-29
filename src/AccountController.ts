2bcc8154d9d9c5f19af510f8edf19a8471fa6820
clevanilson <clevanilson.contato@gmail.com>
2024-03-28 21:14:40 -0300

    feat(http): add account controller

---
+++ src/AccountController.ts
@@ -0,0 +1,22 @@
+import { GetAccountById } from "./GetAccount";
+import { HttpServer } from "./HttpServer";
+import { Signup } from "./Signup";
+
+export class AccountController {
+  constructor(
+    private readonly httpServer: HttpServer,
+    private readonly signup: Signup,
+    private readonly getAccountById: GetAccountById
+  ) {
+    this.httpServer.register("post", "/signup", (_params: any, body: any) => {
+      return this.signup.execute(body);
+    });
+    this.httpServer.register(
+      "get",
+      "/accounts/:id",
+      (params: any, _body: any) => {
+        return this.getAccountById.execute(params.id);
+      }
+    );
+  }
+}
