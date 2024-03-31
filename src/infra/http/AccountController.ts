3a9ed608e0ae4768eee5949991f4cae414b84cb0
Clevanilson da Silva Oliveira <clevanilson.contato@gmail.com>
2024-03-31 12:41:54 -0300

    Feat/class 05 di (#30)
    * feat(di): add `Registry` class and `inject` anotation
    
    * refactor(account): `AccountController` get the usecases via anotation
    
    * refactor(ride): `RideController` get uses cases from anotation

--- src/infra/http/AccountController.ts
+++ src/infra/http/AccountController.ts
@@ -1,21 +1,21 @@
 import { GetAccountById } from "@/application/usecases/GetAccount";
 import { HttpServer } from "@/infra/http/HttpServer";
 import { Signup } from "@/application/usecases/Signup";
+import { inject } from "@/infra/di/Registry";
 
 export class AccountController {
-  constructor(
-    private readonly httpServer: HttpServer,
-    private readonly signup: Signup,
-    private readonly getAccountById: GetAccountById
-  ) {
+  @inject("signup") signup?: Signup;
+  @inject("getAccountById") getAccountById?: GetAccountById;
+
+  constructor(private readonly httpServer: HttpServer) {
     this.httpServer.register("post", "/signup", (_params: any, body: any) => {
-      return this.signup.execute(body);
+      return this.signup?.execute(body);
     });
     this.httpServer.register(
       "get",
       "/accounts/:id",
       (params: any, _body: any) => {
-        return this.getAccountById.execute(params.id);
+        return this.getAccountById?.execute(params.id);
       }
     );
   }
