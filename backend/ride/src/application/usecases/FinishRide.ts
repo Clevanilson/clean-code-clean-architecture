dd5a42ed3e2f403e1069e64f0a700fccbae0312e
clevanilson <clevanilson.contato@gmail.com>
2024-04-07 13:50:57 -0300

    feat(ride): `FinishRide` now publish to the queue

--- backend/ride/src/application/usecases/FinishRide.ts
+++ backend/ride/src/application/usecases/FinishRide.ts
@@ -2,6 +2,7 @@ import { RideRepository } from "@/infra/repositories/RideRepository";
 import { ProcessPayment } from "./ProcessPayment";
 import { Mediator } from "@/infra/mediator/Mediator";
 import { Queue } from "@/infra/queue/Queue";
+import { RideCompletedEvent } from "@/domain/events/RideCompleted";
 
 export class FinishRide {
   constructor(
@@ -12,8 +13,10 @@ export class FinishRide {
   async execute(id: string): Promise<void> {
     const ride = await this.rideRepository.getById(id);
     if (!ride) throw new Error("Ride not found");
+    ride.register(RideCompletedEvent.name, async (event) => {
+      await this.rideRepository.update(ride);
+      this.queue.publish(RideCompletedEvent.name, event);
+    });
     ride.finish();
-    await this.rideRepository.update(ride);
-    await this.queue.publish("rideCompleted", { rideId: ride.rideId });
   }
 }
