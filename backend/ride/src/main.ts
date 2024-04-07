6e3c8843d147f8155d8929fc5a903462d178a258
clevanilson <clevanilson.contato@gmail.com>
2024-04-07 13:51:47 -0300

    feat(ride): add Queue to the DI

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -7,16 +7,29 @@ import { RideRepositoryDatebase } from "@/infra/repositories/RideRepositoryDateb
 import { Registry } from "./infra/di/Registry";
 import { AccountGatewayHttp } from "./infra/gateways/AccountGatewayHttp";
 import { AxiosAdapter } from "./infra/http/AxiosAdapter";
+import { RabbitMQAdapter } from "./infra/queue/RabbitMQAdapter";
+import { ProcessPayment } from "./application/usecases/ProcessPayment";
+import { QueueController } from "./infra/queue/QueueController";
 
-const registry = Registry.getInstance();
-const httpServer = new ExpressAdapter();
-const connection = new PGAdapter();
-const httpClient = new AxiosAdapter();
-const accountGateway = new AccountGatewayHttp(httpClient);
-const rideRepository = new RideRepositoryDatebase(connection);
-const requestRide = new RequestRide(rideRepository, accountGateway);
-const getRide = new GetRide(rideRepository);
-registry.register("requestRide", requestRide);
-registry.register("getRide", getRide);
-new RideController(httpServer);
-httpServer.listen(3001);
+async function main(): Promise<void> {
+  const registry = Registry.getInstance();
+  const httpServer = new ExpressAdapter();
+  const connection = new PGAdapter();
+  const httpClient = new AxiosAdapter();
+  const accountGateway = new AccountGatewayHttp(httpClient);
+  const rideRepository = new RideRepositoryDatebase(connection);
+  const requestRide = new RequestRide(rideRepository, accountGateway);
+  const getRide = new GetRide(rideRepository);
+  const processPayment = new ProcessPayment();
+  const queue = new RabbitMQAdapter();
+  await queue.connect();
+  registry.register("queue", queue);
+  registry.register("processPayment", processPayment);
+  registry.register("requestRide", requestRide);
+  registry.register("getRide", getRide);
+  new RideController(httpServer);
+  new QueueController();
+  httpServer.listen(3001);
+}
+
+main();
